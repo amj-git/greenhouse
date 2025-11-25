25cb85204357273ffcfd25b507c7a203e48922c2
Adrian Jones <amj4321@gmail.com>
2025-11-25 21:11:34 +0000

    Fixed backup script to work in home folder, so it runs under cron

--- backup_onedrive.sh
+++ backup_onedrive.sh
@@ -1,14 +1,14 @@
-
+cd /home/pi/greenhouse
 for f in ${1:-./db/*.db}
 do
-	echo "Backing up $f------------------------"
-	bf="${f%.*}-backup.db"
-	echo "Creating backup file $bf"
-	sqlite3 "$f" ".timeout 10000" ".backup '$bf'"
-	echo "Copying $bf to gh_backup_onedrive:gh_backup"
-	rclone copy "$bf" gh_backup_onedrive:gh_backup
-	echo "Deleting $bf"
-	rm "$bf"
-	echo "Backing up $f COMPLETE--------------------"
+    echo "Backing up $f------------------------"
+    bf="${f%.*}-backup.db"
+    echo "Creating backup file $bf"
+    sqlite3 "$f" ".timeout 10000" ".backup '$bf'"
+    echo "Copying $bf to gh_backup_onedrive:gh_backup"
+    rclone copy "$bf" gh_backup_onedrive:gh_backup
+    echo "Deleting $bf"
+    rm "$bf"
+    echo "Backing up $f COMPLETE--------------------"
 
 done
